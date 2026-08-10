9c4cf8f9cd7566e0dc8cce4cf9e67b74dd6bef5e
Natalia <natycsoto9@gmail.com>
2026-08-10 15:36:03 -0300

    Clase 05. LocalStorage

--- JS/Alumnos.js
+++ JS/Alumnos.js
@@ -26,6 +26,46 @@ iniciar()
 
 function obtenerMaterias(){
     return new Promise((resolve)=>{
-        setTimeout(()=> {})
+        setTimeout(()=> {
+        resolve (materias)
+    }, 3000);
     })
+}
+async function iniciar() {
+    const datos = await obtenerMaterias()
+    console.table(datos)
+}
+iniciar()
+function obtenerDocentes(){
+    return new Promise((resolve)=>{
+     setTimeout(() => {
+        resolve (docente)
+    }, 3000);
+})
+}
+async function iniciar() {
+    const datos= await obtenerDocentes()
+    console.table(datos)
+    
+}
+iniciar()
+
+
+//ejemplo:06/08/26
+async function  prueba() {
+const respuesta = await fetch("https://jsonplaceholder.typicode.com/users")
+console.log(respuesta)
+}
+prueba()
+//10/08/26
+function mostrarAlumnos(alumnos){ 
+    console.log(typeof alumnos)
+    localStorage.setItem("alumnos", JSON.stringify (alumnos))
+    const datosa = localStorage.getItem("alumnos")
+    console.log(typeof datos)
+    console.log (datos)
+    const alumnosRecuperados = JSON.parse(datos)
+    console.log(typeof alumnosRecuperados)
+    console.table(alumnos)
+    
 }
