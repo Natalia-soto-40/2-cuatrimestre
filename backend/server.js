7008657c97eda4bcbe0fcd831b832e1cfbdceffc
Natalia <natycsoto9@gmail.com>
2026-08-27 16:13:36 -0300

    correccion de errores de la clase 14

--- backend/server.js
+++ backend/server.js
@@ -20,48 +20,45 @@ const docentes = [{
     nombre: "Clara",
     Especialidad: "Biologia",
 
-}]
-[{
+},
+{
 id : 2,
 nombre: "Bianca",
 Especialidad: "Matematica"
-}]
-[{
+},
+{
     id: 3,
     nombre: "Valentina",
     Especialidad: "Ciencias de la Educacion",
-}]
-[{
+},
+{
     id: 4,
-    nombre: Ludmila,
+    nombre: "Ludmila",
     Especialidad: "Derecho",
-}]
-[{ 
+},
+{ 
     id: 5,
     nombre: "Cecilia",
-    especialidad:"Geografia",
+    Especialidad:"Geografia",
 }]
 
 
-pp.get("/alumnos", (req, res)=>{
+app.get("/alumnos", (req, res)=>{
     res.json(alumnos)
 })
 app.get("/alumnos/:id", (req, res)=> {
     const id = Number(req.params.id)
     const alumno = alumnos.find (a => a.id === id)
-    res.json(alumno)
+    res.json(alumno)})
 app. listen(3000, () => {
     console.log("servidor funcionando en http://localhost:3000")
 })
-})
-pp.get("/docentes", (req, res)=>{
-    res.json(docentes)
-})
+
 app.get("/docentes", (req, res)=>{
     res.json(docentes)
 })
 app.get("/docentes/:id", (req, res)=> {
     const id = Number(req.params.id)
-    const docente = docentes.find (a => a.id === id)
+    const docente = docentes.find(a => a.id === id)
     res.json(docente)
 })
